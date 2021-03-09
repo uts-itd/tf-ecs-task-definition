b9258cf8f80a6953f91fa81194b9e3f587ace5ca
Vidhi Virmani <vidhi.virmani@utes.edu.au>
2021-03-09 15:22:31 +1100

    locals file updated to accomodate template

--- locals.tf
+++ locals.tf
@@ -23,3 +23,40 @@ locals {
   )
   systemControls = jsonencode(var.systemControls)
 }
+
+data "template_file" "container_definition" {
+  template = file("${path.module}/templates/container-definition.json.tpl")
+
+  vars = {
+    command                = local.command == "[]" ? "null" : local.command
+    cpu                    = var.cpu == 0 ? "null" : var.cpu
+    disableNetworking      = var.disableNetworking ? true : false
+    dnsSearchDomains       = local.dnsSearchDomains == "[]" ? "null" : local.dnsSearchDomains
+    dnsServers             = local.dnsServers == "[]" ? "null" : local.dnsServers
+    dockerLabels           = local.dockerLabels == "{}" ? "null" : local.dockerLabels
+    dockerSecurityOptions  = local.dockerSecurityOptions == "[]" ? "null" : local.dockerSecurityOptions
+    entryPoint             = local.entryPoint == "[]" ? "null" : local.entryPoint
+    environment            = local.environment == "[]" ? "null" : local.environment
+    essential              = var.essential ? true : false
+    extraHosts             = local.extraHosts == "[]" ? "null" : local.extraHosts
+    healthCheck            = local.healthCheck == "{}" ? "null" : local.healthCheck
+    hostname               = var.hostname == "" ? "null" : var.hostname
+    image                  = var.image == "" ? "null" : var.image
+    interactive            = var.interactive ? true : false
+    logConfiguration       = local.logConfiguration == "{}" ? "null" : local.logConfiguration
+    memory                 = var.memory == 0 ? "null" : var.memory
+    memoryReservation      = var.memoryReservation == 0 ? "null" : var.memoryReservation
+    mountPoints            = local.mountPoints == "[]" ? "null" : local.mountPoints
+    name                   = var.name == "" ? "null" : var.name
+    portMappings           = local.portMappings == "[]" ? "null" : local.portMappings
+    privileged             = var.privileged ? true : false
+    pseudoTerminal         = var.pseudoTerminal ? true : false
+    readonlyRootFilesystem = var.readonlyRootFilesystem ? true : false
+    repositoryCredentials  = local.repositoryCredentials == "{}" ? "null" : local.repositoryCredentials
+    resourceRequirements   = local.resourceRequirements == "[]" ? "null" : local.resourceRequirements
+    secrets                = local.secrets == "[]" ? "null" : local.secrets
+    systemControls         = local.systemControls == "[]" ? "null" : local.systemControls
+    user                   = var.user == "" ? "null" : var.user
+    workingDirectory       = var.workingDirectory == "" ? "null" : var.workingDirectory
+  }
+}
